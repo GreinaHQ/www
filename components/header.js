207c7b7f7d992f2b5ebb2fa4b8b3d22377eee98c
Ronny Haase <ronnyhaase@fastmail.com>
2019-04-03 13:16:26 +0100

    Make header logo a link to homepage

--- components/header.js
+++ components/header.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Link from 'next/link'
 
 const Header = () => (
   <header className="Header">
     <div className="Container u-flex-row-left-center">
-      <img alt="Greina Logo" className="u-mr-xs" src="/static/images/logo-100.svg" width="32" />
-      <strong>Greina</strong><span>&nbsp;| Newsletter</span>
+      <Link href="/">
+        <span style={{ cursor: 'pointer' }}>
+          <img alt="Greina Logo" className="u-mr-xs" src="/static/images/logo-100.svg" width="32" />
+          <strong style={{ verticalAlign: 'middle' }}>Greina</strong>
+        </span>
+      </Link>
+      <span>&nbsp;| Newsletter</span>
     </div>
   </header>
 )
