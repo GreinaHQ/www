82418031166c58eae9b1349ea85f3d08974f16b0
Ronny Haase <ronnyhaase@fastmail.com>
2019-04-03 11:27:46 +0100

    Use ES module syntax in config.js

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
-module.exports = {
-  defaultDescription: 'We love great Banking, Money Management and Investing, naturally we like FinTech Startups. Join our newsletter and follow us on Twitter @GreinaHQ!',
-  defaultPreviewImageAlt: 'Greina - We ❤ FinTech - Join our newsletter!',
-  defaultPreviewImageUrl: 'https://greina.io/static/images/preview-1200.jpg',
-  defaultTitle: 'Greina - We &hearts; FinTech',
-  defaultUrl: 'https://greina.io',
+const defaultDescription = 'We love great Banking, Money Management and Investing, naturally we like FinTech Startups. Join our newsletter and follow us on Twitter @GreinaHQ!'
+const defaultPreviewImageAlt = 'Greina - We ❤ FinTech - Join our newsletter!'
+const defaultPreviewImageUrl = 'https://greina.io/static/images/preview-1200.jpg'
+const defaultTitle = 'Greina - We &hearts; FinTech'
+const defaultUrl = 'https://greina.io'
+
+export {
+  defaultDescription,
+  defaultPreviewImageAlt,
+  defaultPreviewImageUrl,
+  defaultTitle,
+  defaultUrl,
 }
